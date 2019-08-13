b39a96bbd124ebcab660f93fe26f92eb3929f8f0
Anshul Singhvi <asinghvi17@simons-rock.edu>
2019-08-13 07:52:36 +0530

    Implement etacov
    comment some stuff
    
    fix iscategorical for int vector

--- src/Pumas.jl
+++ src/Pumas.jl
@@ -8,6 +8,8 @@ using DiffEqDiffTools, Reexport, StatsBase,
 using AdvancedHMC: DiagEuclideanMetric, Hamiltonian, NUTS, Leapfrog, find_good_eps, StanHMCAdaptor, Preconditioner, NesterovDualAveraging
 using MCMCChains: Chains
 
+using StatsPlots
+
 import DataInterpolations
 
 @reexport using OrdinaryDiffEq, Unitful
