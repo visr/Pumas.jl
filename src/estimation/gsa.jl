765de6d48080e37808650de8d1b0322866448c7e
Vaibhavdixit02 <vaibhavyashdixit@gmail.com>
2019-09-04 21:25:04 +0530

    Add dispatch for subject and update to gsa function

--- src/estimation/gsa.jl
+++ src/estimation/gsa.jl
@@ -1,20 +1,41 @@
-function DiffEqSensitivity.gsa(m::PumasModel,data::Population,params::NamedTuple,method::Sobol,p_range=[[0.0,1.0] for i in 1:length(TransformVariables.inverse(toidentitytransform(m.param),params))],N=1000,order=[0],args...; kwargs...)
+function DiffEqSensitivity.gsa(m::PumasModel,subject::Subject,params::NamedTuple,method::DiffEqSensitivity.Sobol,p_range=[[0.0,1.0] for i in 1:length(TransformVariables.inverse(toidentitytransform(m.param),params))],order=[0],args...; N=1000,kwargs...)
     trf_ident = toidentitytransform(m.param)
     function f(p)
         param = TransformVariables.transform(trf_ident,p)
-        sim = simobs(m,data,param)
-        collect(Iterators.flatten([sim.sims[i].observed.dv for i in 1:length(sim.sims)]))
+        sim = simobs(m,subject,param)
+        sim.observed.dv
     end
-    DiffEqSensitivity.sobol_sensitivity(f,p_range,N,order,args...; kwargs...)
+    DiffEqSensitivity.gsa(f,p_range,method,N,order,args...; kwargs...)
 end
 
-function DiffEqSensitivity.gsa(m::PumasModel,data::Population,params::NamedTuple,method::Morris,p_range=[[0.0,1.0] for i in 1:length(TransformVariables.inverse(toidentitytransform(m.param),params))],
+function DiffEqSensitivity.gsa(m::PumasModel,subject::Subject,params::NamedTuple,method::DiffEqSensitivity.Morris,p_range=[[0.0,1.0] for i in 1:length(TransformVariables.inverse(toidentitytransform(m.param),params))],
                 p_steps=[100 for i in 1:length(TransformVariables.inverse(toidentitytransform(m.param),params))],args...; kwargs...)
     trf_ident = toidentitytransform(m.param)
     function f(p)
         param = TransformVariables.transform(trf_ident,p)
-        sim = simobs(m,data,param)
+        sim = simobs(m,subject,param)
+        sim.observed.dv
+    end
+    DiffEqSensitivity.gsa(f,p_range,method,p_steps,args...; kwargs...)
+end
+
+function DiffEqSensitivity.gsa(m::PumasModel,population::Population,params::NamedTuple,method::DiffEqSensitivity.Sobol,p_range=[[0.0,1.0] for i in 1:length(TransformVariables.inverse(toidentitytransform(m.param),params))],N=1000,order=[0],args...; kwargs...)
+    trf_ident = toidentitytransform(m.param)
+    function f(p)
+        param = TransformVariables.transform(trf_ident,p)
+        sim = simobs(m,population, param)
         collect(Iterators.flatten([sim.sims[i].observed.dv for i in 1:length(sim.sims)]))
     end
-    DiffEqSensitivity.morris_sensitivity(f,p_range,p_steps,args...; kwargs...)
+    DiffEqSensitivity.gsa(f,p_range,method,N,order,args...; kwargs...)
+end
+
+function DiffEqSensitivity.gsa(m::PumasModel,population::Population,params::NamedTuple,method::DiffEqSensitivity.Morris,p_range=[[0.0,1.0] for i in 1:length(TransformVariables.inverse(toidentitytransform(m.param),params))],
+                p_steps=[100 for i in 1:length(TransformVariables.inverse(toidentitytransform(m.param),params))],args...; kwargs...)
+    trf_ident = toidentitytransform(m.param)
+    function f(p)
+        param = TransformVariables.transform(trf_ident,p)
+        sim = simobs(m,population, param)
+        collect(Iterators.flatten([sim.sims[i].observed.dv for i in 1:length(sim.sims)]))
+    end
+    DiffEqSensitivity.gsa(f,p_range,method,p_steps,args...; kwargs...)
 end
