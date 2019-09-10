34d07f8894c361b0dbfbc306843751947a7a58f4
Vaibhavdixit02 <vaibhavyashdixit@gmail.com>
2019-09-10 12:25:34 +0530

    Take vector of variables to use for gsa

--- src/estimation/gsa.jl
+++ src/estimation/gsa.jl
@@ -1,19 +1,19 @@
-function DiffEqSensitivity.gsa(m::PumasModel,subject::Subject,params::NamedTuple,method::DiffEqSensitivity.GSAMethod,p_range=[[0.0,1.0] for i in 1:length(TransformVariables.inverse(toidentitytransform(m.param),params))],args...;kwargs...)
+function DiffEqSensitivity.gsa(m::PumasModel,subject::Subject,params::NamedTuple,method::DiffEqSensitivity.GSAMethod,vars = [:dv],p_range=[[0.0,1.0] for i in 1:length(TransformVariables.inverse(toidentitytransform(m.param),params))],args...;kwargs...)
     trf_ident = toidentitytransform(m.param)
     function f(p)
         param = TransformVariables.transform(trf_ident,p)
         sim = simobs(m,subject,param,args...;kwargs...)
-        collect(Iterators.flatten([getproperty(sim.observed,key) for key in keys(sim.observed)])) 
+        collect(Iterators.flatten([getproperty(sim.observed,key) for key in vars])) 
     end
     DiffEqSensitivity.gsa(f,p_range,method)
 end
 
-function DiffEqSensitivity.gsa(m::PumasModel,population::Population,params::NamedTuple,method::DiffEqSensitivity.GSAMethod,p_range=[[0.0,1.0] for i in 1:length(TransformVariables.inverse(toidentitytransform(m.param),params))],args...; kwargs...)
+function DiffEqSensitivity.gsa(m::PumasModel,population::Population,params::NamedTuple,method::DiffEqSensitivity.GSAMethod,vars = [:dv],p_range=[[0.0,1.0] for i in 1:length(TransformVariables.inverse(toidentitytransform(m.param),params))],args...; kwargs...)
     trf_ident = toidentitytransform(m.param)
     function f(p)
         param = TransformVariables.transform(trf_ident,p)
         sim = simobs(m,population,param,args...;kwargs...)
-        collect(Iterators.flatten([collect(Iterators.flatten([getproperty(sim.sims[i].observed,key) for key in keys(sim.sims[1].observed)])) for i in 1:length(sim.sims)]))
+        collect(Iterators.flatten([collect(Iterators.flatten([getproperty(sim.sims[i].observed,key) for key in vars])) for i in 1:length(sim.sims)]))
     end
     DiffEqSensitivity.gsa(f,p_range,method)
 end
