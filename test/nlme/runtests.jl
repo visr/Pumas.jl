d2681c696466287a911f3a166b51948d81808fa6
Patrick Kofod Mogensen <patrick.mogensen@gmail.com>
2019-09-11 17:16:43 +0200

    Fix NLME test sets that didn't run unless ALL was specified. (#674)
    * Fix NLME test sets that didn't run unless ALL was specified.
    
    * No nmtran.
    
    * Change branching in runtests.jl to include all NLME groups
    
    * Fix Bayesian tests and make Bayesian fit method more readable

--- test/nlme/runtests.jl
+++ test/nlme/runtests.jl
@@ -2,11 +2,10 @@ using Test, SafeTestsets
 using Pumas, LinearAlgebra, Optim, StatsBase
 
 if group == "All" || group == "NLME_Basic"
-  @time @safetestset "Types (constructors, api, etc...)"             begin include("types.jl")                  end
-end
-
-if group == "All" || group == "NLME_Naive"
-  @time @safetestset "Naive estimation"                              begin include("single_subject.jl")                  end
+  @time @safetestset "Maximum-likelihood interface" begin
+    @time @safetestset "Types (constructors, api, etc...)"           begin include("types.jl")                     end
+    @time @safetestset "Naive estimation"                            begin include("single_subject.jl")            end
+  end
 end
 
 if group == "All" || group == "NLME_ML1"
