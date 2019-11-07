85dc4c8db59e97dfea0a67eb5541013114718679
Yingbo Ma <mayingbo5@gmail.com>
2019-11-07 17:24:51 -0500

    Fix tests

--- test/nca/singledose_tests.jl
+++ test/nca/singledose_tests.jl
@@ -3,16 +3,14 @@ using Pumas
 
 file = Pumas.example_data("nca_test_data/dapa_IV")
 data = copy(CSV.read(file))
+rawdata = data
 
 timeu = u"hr"
 concu = u"mg/L"
 amtu  = u"mg"
 data[!,:route] .= "iv"
-didxs = findall(!iszero, data.AMT_IV)
-data[rand(didxs), :AMT_IV] = 10
 ncapop = @test_nowarn read_nca(data, id=:ID, time=:TIME, conc=:CObs, amt=:AMT_IV, route=:route,
                                     llq=0concu, timeu=timeu, concu=concu, amtu=amtu)
-@test ustrip.(NCA.doseamt(ncapop)[!, end]) == data[didxs, :AMT_IV]
 @test_nowarn NCA.auc(ncapop, method=:linuplogdown)
 @test all(ismissing, NCA.bioav(ncapop, ithdose=1)[!, 2])
 @test_logs (:warn, "No dosage information has passed. If the dataset has dosage information, you can pass the column names by `amt=:AMT, route=:route`.") NCA.auc(read_nca(data, id=:ID, time=:TIME, conc=:CObs));
@@ -193,3 +191,9 @@ subj = read_nca(df, verbose=false)[1]
 rename!(df, :blq => :_blq)
 subj = read_nca(df, verbose=false)[1]
 @test subj.time == [1; 2;findall(!iszero, df.conc);7]
+
+didxs = findall(!iszero, rawdata.AMT_IV)
+rawdata[rand(didxs), :AMT_IV] = 10
+_ncapop = @test_nowarn read_nca(rawdata, id=:ID, time=:TIME, conc=:CObs, amt=:AMT_IV, route=:route,
+                                    llq=0concu, timeu=timeu, concu=concu, amtu=amtu)
+@test ustrip.(NCA.doseamt(_ncapop)[!, end]) == rawdata[didxs, :AMT_IV]
