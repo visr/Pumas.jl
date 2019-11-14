671e3d8b9902e822d00cdcec9f6c90fd6092fe38
Patrick Kofod Mogensen <patrick.mogensen@gmail.com>
2019-11-14 23:38:53 +0100

    And test for has_syms.

--- test/core/analytical_models.jl
+++ test/core/analytical_models.jl
@@ -1,5 +1,8 @@
 using Pumas
 
-ImmediateAbsorptionModel().syms == [:Central]
-OneCompartmentModel().syms == [:Depot, :Central]
-OneCompartmentParallelModel().syms == [:Depot1, :Depot2, :Central]
+@test ImmediateAbsorptionModel().syms == [:Central]
+@test Pumas.DiffEqBase.has_syms(ImmediateAbsorptionModel())
+@test OneCompartmentModel().syms == [:Depot, :Central]
+@test Pumas.DiffEqBase.has_syms(OneCompartmentModel())
+@test OneCompartmentParallelModel().syms == [:Depot1, :Depot2, :Central]
+@test Pumas.DiffEqBase.has_syms(OneCompartmentParallelModel())
